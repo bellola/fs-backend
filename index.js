43c1220088b7f032f406bacfc11f0e757263488f
Luis A Bello <bellola@Luiss-MBP.lan1>
2020-01-15 11:46:15 -0500

    res.send sending age in a string

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = 3000
 
 
 app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: false}))
 app.use(bodyParser.raw({limit:'50mb', extended: true}));
 
 
@@ -24,7 +24,73 @@ app.use(bodyParser.raw({limit:'50mb', extended: true}));
 
  var rekognition = new AWS.Rekognition()
 
- function analize(image){
+
+//  function _base64ToArrayBuffer(base64) {
+//   var binary_string = atob(base64);
+//   var len = binary_string.length;
+//   var bytes = new Uint8Array(len);
+//   for (var i = 0; i < len; i++) {
+//       bytes[i] = binary_string.charCodeAt(i);
+//   }
+//   return bytes.buffer;
+// }
+
+
+//  function analize(image){
+//   var params = {
+//     Image: { /* required */
+//     //   Bytes: Buffer.from('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
+//       Bytes: image
+//     },
+//     Attributes: [
+//      "ALL"
+//     ]
+//   };
+
+  
+//    rekognition.detectFaces(params, function(err, data) {
+//     if (err) {
+//        console.log(err, err.stack); // an error occurred
+//     }
+    
+   
+//     else{     
+      
+//       console.log("you are " + (data.FaceDetails[0].AgeRange.Low + data.FaceDetails[0].AgeRange.High)/2 + " years old");    
+      
+//       return "you are " + (data.FaceDetails[0].AgeRange.Low + data.FaceDetails[0].AgeRange.High)/2 + " years old"
+      
+      
+//     }// successful response
+//   })
+//  }
+
+ 
+
+
+  
+  
+app.use(express.static('public'))
+  
+  app.post('/test',  function(req, res){
+   
+
+  let img = req.body.request
+
+  function _base64ToArrayBuffer(image) {
+    var binary_string = atob(image);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+        bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
+
+  let imgArr = _base64ToArrayBuffer(img)
+
+  
+  function whatsYourAge(image){
   var params = {
     Image: { /* required */
     //   Bytes: Buffer.from('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
@@ -34,50 +100,28 @@ app.use(bodyParser.raw({limit:'50mb', extended: true}));
      "ALL"
     ]
   };
-  rekognition.detectFaces(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log("you are " + (data.FaceDetails[0].AgeRange.Low + data.FaceDetails[0].AgeRange.High)/2 + " years old");           // successful response
-    // else console.log(data.FaceDetails[0].Emotions)
-  }); 
- }
-
- function _base64ToArrayBuffer(base64) {
-  var binary_string = atob(base64);
-  var len = binary_string.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++) {
-      bytes[i] = binary_string.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
- 
 
   
-  
-app.use(express.static('public'))
-  
-  app.post('/test',async function(req, res){
-    try {
-      // console.log('HITTING IT')
-      // console.log(_base64ToArrayBuffer(req.body.request))
-      // console.log(req.body.request)
-      if (analize(_base64ToArrayBuffer(req.body.request))){
-
-        res.send(result)
-      } else {
-        res.send('nope')
-      }
-      
-    
-      // res.send('HELLO WORLD')
-    } catch (error) {
-      
-      console.log(error)
+   rekognition.detectFaces(params, function(err, data) {
+    if (err) {
+       console.log(err, err.stack); // an error occurred
     }
     
+   
+    else{     
+      
+      console.log("you are " + (data.FaceDetails[0].AgeRange.Low + data.FaceDetails[0].AgeRange.High)/2 + " years old");    
+      
+     res.send( "you are " + (data.FaceDetails[0].AgeRange.Low + data.FaceDetails[0].AgeRange.High)/2 + " years old")
+      
+      
+    }// successful response
+  })
+}
+ 
 
-    
+whatsYourAge(imgArr)
+
     })
   
 
